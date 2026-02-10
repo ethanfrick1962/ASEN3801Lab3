423df9b27fbd097ff302b21cd776ff084f0bdfeb
Ethan Frick <ethanfrick1962@outlook.com>
2026-02-10 14:24:53 -0700

    Some changes

--- data_cleanup.m
+++ data_cleanup.m
@@ -5,6 +5,12 @@ clear; clc; close all;
 %% Clean All Data
 
 % Collecting all data files
-datafolder = [pwd, 'Data'];
+datafolder = [pwd, '/data'];
 datapattern = fullfile(datafolder, '*');
 datafiles = dir(datapattern);
+
+for i = 1:length(datafiles)
+
+    T = readtable("data", "FileType", "text", 'Delimiter', '\t');
+
+end
