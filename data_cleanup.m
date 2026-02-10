6b07fd4cd34502f6de7cbc57381fb8efc6690151
Ethan Frick <ethanfrick1962@outlook.com>
2026-02-10 14:42:11 -0700

    Cleaned data

--- data_cleanup.m
+++ data_cleanup.m
@@ -8,9 +8,24 @@ clear; clc; close all;
 datafolder = [pwd, '/data'];
 datapattern = fullfile(datafolder, '*');
 datafiles = dir(datapattern);
+datafiles = datafiles(~ismember({datafiles.name},{'.','..'}));
+data = struct();
 
+% Iterate through all tables
 for i = 1:length(datafiles)
+    % Read and clean data
+    T = readmatrix([datafiles(i).folder '/' datafiles(i).name], "FileType", "text", 'Delimiter', '\t');
+    rmmissing(T);
 
-    T = readtable("data", "FileType", "text", 'Delimiter', '\t');
+    % Name file
+    name = erase(datafiles(i).name, '2026_02_10_002_');
 
+    % Add table to data struct
+    data.(['Data_' name]) = T;
 end
+
+% Clean all other data
+clear datafolder datapattern datafiles i name T ans;
+
+% Save as MATLAB workspace
+save('CleanData.mat');
